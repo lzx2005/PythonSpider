87c4779f3e287dafaf1f7e32028baf0fd76db912
crow2005@vip.qq.com <crow2005@vip.qq.com>
2016-07-12 13:29:21 +0800

    first

---
+++ baike_spider/spider_main.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+from baike_spider import url_manager
+from baike_spider import html_downloader
+from baike_spider import html_parser
+
+from baike_spider import html_outputer
+
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls = url_manager.UrlManager()
+        self.downloader = html_downloader.HtmlDownloader()
+        self.parser = html_parser.HtmlParser()
+        self.outputer = html_outputer.HtmlOutputer()
+
+    def craw(self, root_url):
+        pass
+
+
+if __name__ == "__main__":
+    root_url = "http://baike.baidu.com/view/21087.htm"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
